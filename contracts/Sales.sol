81025d5d28a29e7dfce587b75ae0cbce82e4b8a9
Matthew Black <mblackmblackmblack@gmail.com>
2019-07-26 15:58:59 -0400

    Improve logic for Sales.take

--- contracts/Sales.sol
+++ contracts/Sales.sol
@@ -263,23 +263,19 @@ contract Sales is DSMath { // Auctions
         sales[sale].taken = true;
 
         uint256 available = add(sales[sale].bid, loans.back(sales[sale].loani));
+        uint256 amount = min(available, loans.lent(sales[sale].loani));
 
-        if (available > loans.lent(sales[sale].loani)) {
-            require(tokes[sale].transfer(sales[sale].lend, loans.lent(sales[sale].loani)));
-            available = sub(available, loans.lent(sales[sale].loani));
-        } else {
-            require(tokes[sale].transfer(sales[sale].lend, available));
-            available = 0;
-        }
+        require(tokes[sale].transfer(sales[sale].lend, amount));
+        available = sub(available, amount);
 
-        if (available > add(loans.lfee(sales[sale].loani), loans.lpen(sales[sale].loani))) {
+        if (available >= add(loans.lfee(sales[sale].loani), loans.lpen(sales[sale].loani))) {
             if (agent(sale) != address(0)) {
                 require(tokes[sale].transfer(sales[sale].agent, loans.lfee(sales[sale].loani)));
             }
             require(tokes[sale].approve(address(med), loans.lpen(sales[sale].loani)));
             med.push(loans.lpen(sales[sale].loani), tokes[sale]);
             available = sub(available, add(loans.lfee(sales[sale].loani), loans.lpen(sales[sale].loani)));
-        } else if (available != 0) {
+        } else if (available > 0) {
             require(tokes[sale].approve(address(med), available));
             med.push(available, tokes[sale]);
             available = 0;
