cea063f64daa4585a3f3a7e41bcab6f9a39f657b
Matthew Black <mblackmblackmblack@gmail.com>
2019-12-22 00:42:04 -0800

    Fix issues with interest increase and decrease

--- test/interestIncrease.js
+++ test/interestIncrease.js
@@ -267,7 +267,7 @@ stablecoins.forEach((stablecoin) => {
             await this.token.transfer(liquidator, toWei('100', unit))
             await this.token.approve(this.loans.address, toWei('100', unit), { from: liquidator })
 
-            await this.loans.liquidate(this.loan, liquidatorSechs[0], ensure0x(liquidatorpbkh), { from: liquidator })
+            await this.loans.liquidate(loan, liquidatorSechs[0], ensure0x(liquidatorpbkh), { from: liquidator })
           }
         }
 
